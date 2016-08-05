8cd233f1c5310ad8ca4fd37c9dd978f559aeea3c
bitweft <bitweft@gmail.com>
2016-08-05 15:03:20 +0530

    Koushik, Lavanya | #1823 | Create distributable file
    * Use webpack for bundling
    * Make react a peer dependency

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,9 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'FormControls'
   },
   externals: {
     'react/lib/ExecutionEnvironment': true,
