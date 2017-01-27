0a4e85744d32edd9e99758ccc4ad1aedcf61fd26
Venkata Jaswanth <aj.jaswanth@gmail.com>
2017-01-27 10:40:59 +0530

    Jaswanth | #3171 | Disable AddMore control property

--- src/components/designer/ObsControlDesigner.jsx
+++ src/components/designer/ObsControlDesigner.jsx
@@ -240,11 +240,6 @@ const descriptor = {
             defaultValue: false,
           },
           {
-            name: 'addMore',
-            dataType: 'boolean',
-            defaultValue: false,
-          },
-          {
             name: 'hideLabel',
             dataType: 'boolean',
             defaultValue: false,
