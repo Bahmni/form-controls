1094efb2571f1109b25028718c8ba82422d30366
xinzhang <xinzhang@thoughtworks.com>
2017-03-08 13:51:18 +0800

    Xin | #3296 | fix error that can't save empty value

--- test/helpers/ObservationMapper.spec.js
+++ test/helpers/ObservationMapper.spec.js
@@ -7,80 +7,81 @@ import ObservationMapper from '../../src/helpers/ObservationMapper';
 chai.use(chaiEnzyme());
 
 describe('ObservationMapper', () => {
-  it('should get obs exclude label record', () => {
-    const obsFormFieldPath = 'ComplexTest.5/23-0';
-    const obsConcept = {
-      answers: [],
-      datatype: 'Boolean',
-      name: 'Smoking History',
-      properties: {
-        allowDecimal: null,
+  const obsFormFieldPath = 'ComplexTest.5/23-0';
+  const obsConcept = {
+    answers: [],
+    datatype: 'Boolean',
+    name: 'Smoking History',
+    properties: {
+      allowDecimal: null,
+    },
+    uuid: 'c2a43174-c9db-4e54-8516-17372c83537f',
+  };
+  const obsDataSource = {
+    concept: obsConcept,
+    formFieldPath: obsFormFieldPath,
+    formNamespace: 'Bahmni',
+    voided: true,
+  };
+  const obsControl = {
+    concept: obsConcept,
+    hiAbsolute: null,
+    hiNormal: null,
+    id: '23',
+    label: {
+      type: 'label',
+      value: 'Smoking History',
+    },
+    lowAbsolute: null,
+    lowNormal: null,
+    options: [
+      {
+        name: 'Yes',
+        value: true,
       },
-      uuid: 'c2a43174-c9db-4e54-8516-17372c83537f',
-    };
-    const obsDataSource = {
-      concept: obsConcept,
-      formFieldPath: obsFormFieldPath,
-      formNamespace: 'Bahmni',
-      voided: true,
-    };
-    const obsControl = {
-      concept: obsConcept,
-      hiAbsolute: null,
-      hiNormal: null,
-      id: '23',
-      label: {
-        type: 'label',
-        value: 'Smoking History',
+      {
+        name: 'No',
+        value: false,
       },
-      lowAbsolute: null,
-      lowNormal: null,
-      options: [
-        {
-          name: 'Yes',
-          value: true,
-        },
-        {
-          name: 'No',
-          value: false,
-        },
-      ],
-      properties: {
-        addMore: false,
-        hideLabel: false,
-        location: {
-          column: 0,
-          row: 0,
-        },
-        mandatory: true,
-        notes: false,
+    ],
+    properties: {
+      addMore: false,
+      hideLabel: false,
+      location: {
+        column: 0,
+        row: 0,
       },
-      type: 'obsControl',
-      units: null,
-    };
+      mandatory: true,
+      notes: false,
+    },
+    type: 'obsControl',
+    units: null,
+  };
+  const labelFormFieldPath = 'ComplexTest.5/27-0';
+  const labelControl = {
+    id: '27',
+    properties: {
+      location: {
+        column: 0,
+        row: 1,
+      },
+    },
+    type: 'label',
+    value: 'This is label test',
+  };
+  const labelDataSource = {
+    formFieldPath: labelFormFieldPath,
+    formNamespace: 'Bahmni',
+    voided: true,
+  };
+
+  it('should get obs exclude record that is voided and doesn\'t have concept', () => {
     const obsRecord = new ControlRecord({
       control: obsControl,
       formFieldPath: obsFormFieldPath,
       value: { value: false, comment: undefined },
       dataSource: obsDataSource,
     });
-    const labelFormFieldPath = 'ComplexTest.5/27-0';
-    const labelControl = {
-      id: '27',
-      properties: {
-        location: {
-          column: 0,
-          row: 1,
-        },
-      },
-      type: 'label',
-      value: 'This is label test',
-    };
-    const labelDataSource = {
-      formFieldPath: labelFormFieldPath,
-      formNamespace: 'Bahmni',
-      voided: true,
-    };
     const labelRecord = new ControlRecord({
       control: labelControl,
       formFieldPath: labelFormFieldPath,
@@ -93,5 +94,26 @@ describe('ObservationMapper', () => {
 
     expect(observations.length).to.equal(1);
     observations.forEach(obs => expect(obs.voided).to.equal(false));
+  });
+
+  it('should get obs include record that is voided but has concept', () => {
+    const obsRecord = new ControlRecord({
+      control: obsControl,
+      formFieldPath: obsFormFieldPath,
+      value: {},
+      dataSource: obsDataSource,
+    });
+    const labelRecord = new ControlRecord({
+      control: labelControl,
+      formFieldPath: labelFormFieldPath,
+      value: {},
+      dataSource: labelDataSource,
+    });
+    const recordTree = new ControlRecord({ children: List.of(obsRecord, labelRecord) });
+
+    const observations = (new ObservationMapper()).from(recordTree);
+
+    expect(observations.length).to.equal(1);
+    observations.forEach(obs => expect(obs.voided).to.equal(true));
   });
 });
