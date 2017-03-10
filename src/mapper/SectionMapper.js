3a7c8b6994b67102c43b29236afc693712e217b7
xinzhang <xinzhang@thoughtworks.com>
2017-03-10 14:54:21 +0800

    Xin | #3296 | fix multiple select that in section can't save error.

--- src/mapper/SectionMapper.js
+++ src/mapper/SectionMapper.js
@@ -1,12 +1,7 @@
-import isEmpty from 'lodash/isEmpty';
-import each from 'lodash/each';
-import filter from 'lodash/filter';
 import { ObsList } from 'src/helpers/ObsList';
 import { List } from 'immutable';
 import { createFormNamespaceAndPath } from 'src/helpers/formNamespace';
 import flattenDeep from 'lodash/flattenDeep';
-import MapperStore from 'src/helpers/MapperStore';
-import { getKeyPrefixForControl } from '../helpers/formNamespace';
 import ObservationMapper from '../helpers/ObservationMapper';
 
 export class SectionMapper {
@@ -19,38 +14,9 @@ export class SectionMapper {
   }
 
   _getInitialObjectInternal(formName, formVersion, control, bahmniObservations) {
-    let obsList = new List();
-    obsList = this.findControls(control, formName, formVersion, bahmniObservations, obsList);
-
+    let obsList = bahmniObservations || new List();
     const { formFieldPath } = createFormNamespaceAndPath(formName, formVersion, control.id);
     return new ObsList({ obsList, formFieldPath });
-  }
-
-  findControls(control, formName, formVersion, bahmniObservations, oldObsList) {
-    let obsList = oldObsList;
-    each(control.controls, (item) => {
-      if (item.type === 'section') {
-        obsList = obsList.push(
-          this._getInitialObjectInternal(formName, formVersion, item, bahmniObservations)
-        );
-      }
-
-      const formFieldPathPrefix =
-        getKeyPrefixForControl(formName, formVersion, item.id).formFieldPath;
-      const filteredObs = filter(bahmniObservations,
-        (observation) => {
-          const obsFormFieldPathPrefix = observation.formFieldPath.split('-')[0];
-          return obsFormFieldPathPrefix === formFieldPathPrefix;
-        }
-      );
-
-      if (!isEmpty(filteredObs)) {
-        const mapper = MapperStore.getMapper(item);
-        obsList = obsList.push(...mapper.getInitialObject(formName, formVersion,
-          item, filteredObs));
-      }
-    });
-    return obsList;
   }
 
   getValue() {
@@ -64,6 +30,6 @@ export class SectionMapper {
   }
 
   getChildren(data) {
-    return data.obsList.toJS();
+    return data.obsList;
   }
 }
