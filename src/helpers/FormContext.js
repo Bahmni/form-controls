d187ca9b0121286970618a74302addc9e8a0999f
xinzhang <xinzhang@thoughtworks.com>
2017-03-27 10:15:10 +0800

    Xin | #3129 | Spilt code to different files.

---
+++ src/helpers/FormContext.js
@@ -0,0 +1,43 @@
+import ControlRecordTreeMgr from "./ControlRecordTreeMgr";
+
+export default class FormContext {
+
+  constructor(formData) {
+    this.formData = formData;
+  }
+
+  hasContainConcept(record, conceptName) {
+    const concept = record.control && record.control.concept;
+    return concept && concept.name === conceptName;
+  }
+
+  findRecordsByConceptName(recordTree, conceptName) {
+    let recordArray = [];
+    if (this.hasContainConcept(recordTree, conceptName)) {
+      recordArray.push(recordTree);
+    }
+    recordTree.children && recordTree.children.forEach(r => {
+      const filteredArray = this.findRecordsByConceptName(r, conceptName);
+      recordArray = recordArray.concat(filteredArray);
+    });
+    return recordArray;
+  }
+
+  get(conceptName, position=0) {
+    const recordArray = this.findRecordsByConceptName(this.formData, conceptName);
+    return recordArray[position];
+  }
+
+  set(conceptName, position, key, value) {
+    const currentTree = this.get(conceptName, position);
+    if (currentTree) {
+      const updatedCurrentTree = currentTree.set(key, value);
+      this.formData = ControlRecordTreeMgr.update(this.formData, updatedCurrentTree);
+    }
+  }
+
+  getData() {
+    return this.formData;
+  }
+
+}
--- src/helpers/ScriptRunner.js
+++ src/helpers/ScriptRunner.js
@@ -1,46 +1,4 @@
-import ControlRecordTreeMgr from "./ControlRecordTreeMgr";
-
-class FormContext {
-
-  constructor(formData) {
-    this.formData = formData;
-  }
-
-  hasContainConcept(record, conceptName) {
-    const concept = record.control && record.control.concept;
-    return concept && concept.name === conceptName;
-  }
-
-  findRecordsByConceptName(recordTree, conceptName) {
-    let recordArray = [];
-    if (this.hasContainConcept(recordTree, conceptName)) {
-      recordArray.push(recordTree);
-    }
-    recordTree.children && recordTree.children.forEach(r => {
-      const filteredArray = this.findRecordsByConceptName(r, conceptName);
-      recordArray = recordArray.concat(filteredArray);
-    });
-    return recordArray;
-  }
-
-  get(conceptName, position=0) {
-    const recordArray = this.findRecordsByConceptName(this.formData, conceptName);
-    return recordArray[position];
-  }
-
-  set(conceptName, position, key, value) {
-    const currentTree = this.get(conceptName, position);
-    if (currentTree) {
-      const updatedCurrentTree = currentTree.set(key, value);
-      this.formData = ControlRecordTreeMgr.update(this.formData, updatedCurrentTree);
-    }
-  }
-
-  getData() {
-    return this.formData;
-  }
-
-}
+import FormContext from "./FormContext";
 
 export default class ScriptRunner {
 
@@ -51,7 +9,7 @@ export default class ScriptRunner {
   execute(eventJs) {
     const form = this.formContext;
     if (eventJs) {
-      const executiveJs = '(' + eventJs + ')()';
+      const executiveJs = `(${eventJs})()`;
       eval(executiveJs);
     }
     return form.getData();
