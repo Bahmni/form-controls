eb94da0e966971c519c6345bbf1803edcce9bcf2
yaeli <yaeli@thoughtworks.com>
2017-03-30 11:15:25 +0800

    Yae & Xin | #3397 | Clarify label unit test to check class name of the label.

--- test/components/Label.spec.js
+++ test/components/Label.spec.js
@@ -18,14 +18,13 @@ describe('Label', () => {
     const metadata = { value: 'History Notes', type: 'label' };
 
     const wrapper = shallow(<Label enabled={false} metadata={metadata} />);
-    expect(wrapper.find('.disable')).to.have.length(1);
-    expect(wrapper.find('.disable').text()).to.eql('History Notes');
+    expect(wrapper.find('label')).to.have.className('disable');
   });
 
   it('should not set label to class disable when the props of enabled is true', () => {
     const metadata = { value: 'History Notes', type: 'label' };
 
     const wrapper = shallow(<Label enabled metadata={metadata} />);
-    expect(wrapper.find('.disable')).to.have.length(0);
+    expect(wrapper.find('label')).to.not.have.className('disable');
   });
 });
