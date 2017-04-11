69bb7c48f3004793a292b48326b188b98ff9abda
xinzhang <xinzhang@thoughtworks.com>
2017-04-11 14:31:50 +0800

    Xin | #3417 | Modify the css style of popup message to follow the angular one.

--- src/helpers/Notification.js
+++ src/helpers/Notification.js
@@ -1,13 +1,22 @@
 import React, { PropTypes } from 'react';
 
 const NotificationContainer = (props) => {
-  const notificationType = `notification--${props.notification.type}`;
-  return (
-        <div className="notification">
-          <div className={ notificationType }>
-            <div className="message">{ props.notification.message }</div>
+  const messageType = `message-container ${props.notification.type}-message-container`;
+  if (props.notification.message) {
+    return (
+      <div className="messages">
+        <div className={ messageType }>
+          <div className="message-icon">
+            <i className="fa fa-check-circle"></i>
           </div>
-        </div>);
+          <div className="message-text">
+            { props.notification.message }
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (<div></div>);
 };
 
 NotificationContainer.propTypes = {
