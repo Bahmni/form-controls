cf192f2f77cc2003404ba53bcf1180cabcea58b5
yaeli <yaeli@thoughtworks.com>
2017-04-20 08:45:55 +0800

    Yae | #2882 | Can upload the file to server.

--- src/components/ComplexControl.jsx
+++ src/components/ComplexControl.jsx
@@ -3,10 +3,47 @@ import ComponentStore from 'src/helpers/componentStore';
 import { Validator } from 'src/helpers/Validator';
 
 export class ComplexControl extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = (event) => {
+      this._uploadFile(event.target.result, '623bd342-8056-4eb9-8a3e-9bb99e8a62fc').then((response) => response.json())
+        .then(data => {
+          this.props.onChange(data.url);
+        });
+    };
+    reader.readAsDataURL(file);
+  }
+
+  _uploadFile(file, patientUuid) {
+    const searchStr = ';base64';
+    let format;
+    format = file.split(searchStr)[0].split('/')[1];
+
+    const url = 'https://local.mybahmni.org' + '/openmrs/ws/rest/v1/bahmnicore/visitDocument/uploadDocument';
+    return fetch(url, {
+      method: 'POST',
+      headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content: file.substring(file.indexOf(searchStr) + searchStr.length, file.length),
+        format,
+        patientUuid,
+        fileType: 'file',
+      }),
+      credentials: 'include',
+    });
+  }
+
   render() {
     return (
         <div className="obs-comment-section-wrap">
           <input type="file"
+            onChange={(e) => this.handleChange(e)}
           />
         </div>
     );
--- test/components/ComplexControl.spec.js
+++ test/components/ComplexControl.spec.js
@@ -7,25 +7,48 @@ import sinon from 'sinon';
 import { Error } from 'src/Error';
 
 chai.use(chaiEnzyme());
+const sinonStubPromise = require('sinon-stub-promise');
+sinonStubPromise(sinon);
+
 
 describe('ComplexControl', () => {
   let onChangeSpy;
   let wrapper;
   beforeEach(() => {
+    onChangeSpy = sinon.spy();
     wrapper = mount(
       <ComplexControl
         formFieldPath="test1.1/1-0"
         onChange={onChangeSpy}
         validate={false}
         validations={[]}
-      />
-    );
-    onChangeSpy = sinon.spy();
+      />);
   });
 
   it('should render ComplexControl', () => {
     expect(wrapper.find('input')).to.have.prop('type').to.eql('file');
   });
 
+  it('should upload file to server', () => {
+    const response = {
+      json: () => {},
+    };
+    window.fetch = () => {};
+    const fetchPromise = sinon.stub(window, 'fetch').returnsPromise();
+    const responsePromise = sinon.stub(response, 'json').returnsPromise();
+    fetchPromise.resolves(response);
+    responsePromise.resolves({ url: 'someUrl' });
 
+    const result = 'data:image/jpeg;base64,/9j/4SumRXhpZgAATU';
+    const stub = sinon.stub(FileReader.prototype, 'readAsDataURL', function () {
+      this.onloadend({ target: { result } });
+    });
+    const uploadSpy = sinon.spy(wrapper.instance(), '_uploadFile');
+
+    wrapper.find('input').simulate('change', { target: { files: [{}] } });
+
+    sinon.assert.calledOnce(uploadSpy.withArgs(result));
+    sinon.assert.calledOnce(onChangeSpy.withArgs('someUrl'));
+    stub.restore();
+  });
 });
