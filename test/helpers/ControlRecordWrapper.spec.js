ffd3a9f3f261f885cb406047c14cefcbf2c3166d
sowmika148 <37897922+sowmika148@users.noreply.github.com>
2020-01-23 18:00:04 +0530

    BAH-968 | Sowmika | Create hideAndClear method to clear obs when hidden (#16)
    * Sowmika | Create hideAndClear method to clear obs when hidden
    
    * Sowmika | Bump to version 0.91.18

--- test/helpers/ControlRecordWrapper.spec.js
+++ test/helpers/ControlRecordWrapper.spec.js
@@ -55,17 +55,27 @@ describe('ControlRecordWrapper', () => {
   const controlledTree = new ControlRecord({
     control,
     formFieldPath: '3406.1/1-0',
+    value: {
+      value: undefined,
+      comment: 'comment',
+    },
   });
 
   const clonedControlledTree = new ControlRecord({
     control,
     formFieldPath: '3406.1/1-1',
+    value: {
+      value: undefined,
+      comment: 'comment',
+    },
   });
 
   const controlledObsGroupTree = new ControlRecord({
-    control: [{ concept: {
-      name: 'Temperature Data',
-    } }],
+    control: {
+      type: 'obsGroupControl',
+      concept: {
+        name: 'Temperature Data',
+      } },
     children: List.of(clonedControlledTree, controlledTree),
     formFieldPath: '3406.1/3-0',
   });
@@ -111,5 +121,116 @@ describe('ControlRecordWrapper', () => {
     targetWrapper.currentRecord.children.forEach(r => {
       expect(r.enabled).to.equal(false);
     });
+  });
+
+  it('should set value and comments to undefined for obs when the record is hidden', () => {
+    const wrapper = new ControlRecordWrapper(rootTree);
+    const targetWrapper = wrapper.set(controlledTree);
+    targetWrapper.setValue(112);
+
+    targetWrapper.hideAndClear(true);
+
+    const childrenList = targetWrapper.getRecords().children;
+    const controlledRecord = childrenList.get(1);
+    const clonedControlledRecord = childrenList.get(2);
+    expect(controlledRecord.getValue()).to.equal(undefined);
+    expect(clonedControlledRecord.getValue()).to.equal(undefined);
+    expect(controlledRecord.hidden).to.equal(true);
+    expect(clonedControlledRecord.hidden).to.equal(true);
+  });
+
+  it('should set value and comments to undefined for all its children when the obs group is ' +
+    'hidden', () => {
+    const wrapper = new ControlRecordWrapper(rootTree);
+    const targetWrapper = wrapper.set(controlledObsGroupTree);
+    controlledObsGroupTree.children.get(0).value.value = 112;
+    controlledObsGroupTree.children.get(1).value.value = 112;
+
+    targetWrapper.hideAndClear();
+
+    const updatedTree = targetWrapper.rootRecord.children.get(3);
+    expect(updatedTree.hidden).to.eq(true);
+    expect(updatedTree.children.get(0).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(0).value.comment).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.comment).to.eq(undefined);
+  });
+
+  it('should set value and comments to undefined when the table is hidden', () => {
+    const controlledTableTree = new ControlRecord({
+      control: {
+        type: 'table',
+        concept: {
+          name: 'Temperature Data',
+        } },
+      children: List.of(clonedControlledTree, controlledTree),
+      formFieldPath: '3406.1/3-0',
+    });
+    const newRootTree = new ControlRecord({
+      children: List.of(controlledTableTree),
+    });
+    const wrapper = new ControlRecordWrapper(newRootTree);
+    const targetWrapper = wrapper.set(controlledTableTree);
+    controlledTableTree.children.get(0).value.value = 112;
+    controlledTableTree.children.get(1).value.value = 112;
+
+    targetWrapper.hideAndClear();
+
+    const updatedTree = targetWrapper.rootRecord.children.get(0);
+    expect(updatedTree.hidden).to.eq(true);
+    expect(updatedTree.children.get(0).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(0).value.comment).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.comment).to.eq(undefined);
+  });
+
+  it('should set value and comments to undefined when the section is set hidden', () => {
+    const controlledSectionTree = new ControlRecord({
+      control: {
+        type: 'section',
+        concept: {
+          name: 'Temperature Data',
+        } },
+      children: List.of(clonedControlledTree, controlledTree),
+      formFieldPath: '3406.1/3-0',
+    });
+    const newRootTree = new ControlRecord({
+      children: List.of(controlledSectionTree),
+    });
+    const wrapper = new ControlRecordWrapper(newRootTree);
+    const targetWrapper = wrapper.set(controlledSectionTree);
+    controlledSectionTree.children.get(0).value.value = 112;
+    controlledSectionTree.children.get(1).value.value = 112;
+
+    targetWrapper.hideAndClear();
+
+    const updatedTree = targetWrapper.rootRecord.children.get(0);
+    expect(updatedTree.hidden).to.eq(true);
+    expect(updatedTree.children.get(0).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(0).value.comment).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.value).to.eq(undefined);
+    expect(updatedTree.children.get(1).value.comment).to.eq(undefined);
+  });
+
+  it('should set control to hidden when the section is set hidden  and children is null', () => {
+    const controlledSectionTree = new ControlRecord({
+      control: {
+        type: 'section',
+        concept: {
+          name: 'Temperature Data',
+        } },
+      children: null,
+      formFieldPath: '3406.1/3-0',
+    });
+    const newRootTree = new ControlRecord({
+      children: List.of(controlledSectionTree),
+    });
+    const wrapper = new ControlRecordWrapper(newRootTree);
+    const targetWrapper = wrapper.set(controlledSectionTree);
+
+    targetWrapper.hideAndClear();
+
+    const updatedTree = targetWrapper.rootRecord.children.get(0);
+    expect(updatedTree.hidden).to.eq(true);
   });
 });
